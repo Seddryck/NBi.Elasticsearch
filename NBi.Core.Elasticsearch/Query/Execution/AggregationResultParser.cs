7dcdcacc73e7a4528b75fe6574d26117df59f8d3
Cédric L. Charlier <cedric.charlier@yucom.be>
2018-05-01 22:56:10 +0200

    Refactoring for the parsing of a result from Elasticsearch

---
+++ NBi.Core.Elasticsearch/Query/Execution/AggregationResultParser.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.Elasticsearch.Query.Execution
+{
+    class AggregationResultParser : BaseResultParser, IResultParser
+    {
+        protected override bool OnComplexParsing(object jsonField, out object dataField)
+        {
+            if (jsonField is JObject
+                    && (jsonField as JObject).Children<JProperty>().Count() == 1
+                    && (jsonField as JObject).SelectToken("value") != null)
+            {
+                dataField = (jsonField as JObject).SelectToken("value").ToObject<object>();
+                return true;
+            }
+            else
+            {
+                dataField = null;
+                return false;
+            }
+        }
+
+        public bool CanHandle(JObject result)
+            => result.Properties().Any(x => x.Name=="aggregations");
+                
+        protected override IEnumerable<JToken> OnExtractObjects(JToken root)
+            => root.SelectTokens("aggregations.*.buckets").Values().ToArray();
+    }
+}
---
+++ NBi.Core.Elasticsearch/Query/Execution/IResultParser.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.Elasticsearch.Query.Execution
+{
+    interface IResultParser
+    {
+        DataTable Execute(JToken result);
+        bool CanHandle (JObject result);
+    }
+}
